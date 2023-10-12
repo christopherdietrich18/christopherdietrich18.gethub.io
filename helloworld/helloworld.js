372a5f07d925404417e50badb9cb9b42fd55fd87
christopherdietrich18 <144835657+christopherdietrich18@users.noreply.github.com>
2023-10-12 12:48:51 -0500

    Add files via upload

---
+++ helloworld/helloworld.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var h1Element = document.getElementById("helloWorld");
+    if (h1Element) {
+        h1Element.textContent = "Hello, World!";
+    }
+});
